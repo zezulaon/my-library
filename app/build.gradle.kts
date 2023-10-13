3ff0d9f6be840e87170f2aa76f5e7758eb4380f3
zezulaon <1711411+zezulaon@users.noreply.github.com>
2023-10-13 21:07:01 +0200

    Bumps version to same version as "mbd"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.zezula.books"
         minSdk = 23
         targetSdk = 33
-        versionCode = 104
-        versionName = "1.0.4"
+        versionCode = 301
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "dev.zezula.books.InstrumentationTestRunner"
 
