8f860038639030c891059495a25f7a9131698f01
zezulaon <1711411+zezulaon@users.noreply.github.com>
2023-12-04 05:16:25 +0100

    Adds info messages to shelves/notes screens

--- app/src/main/java/dev/zezula/books/ui/screen/detail/TabNotes.kt
+++ app/src/main/java/dev/zezula/books/ui/screen/detail/TabNotes.kt
@@ -1,14 +1,25 @@
 package dev.zezula.books.ui.screen.detail
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import dev.zezula.books.R
 import dev.zezula.books.data.model.note.Note
 
 @Composable
@@ -18,19 +29,54 @@ fun TabNotes(
     onDeleteClick: (Note) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val expandedNoteItem = remember { mutableStateOf<Note?>(null) }
-    LazyColumn(
-        contentPadding = PaddingValues(vertical = 24.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        items(items = uiState.notes, key = { note -> note.id }) { note ->
-            val isExpanded = expandedNoteItem.value == note
-            NoteListItem(
-                note = note,
-                isExpanded = isExpanded,
-                onEditClick = onEditClick,
-                onDeleteClick = onDeleteClick,
-                onExpandClick = { expandedNoteItem.value = if (isExpanded) null else note },
+    Box {
+        if (uiState.notes.isEmpty()) {
+            NoNotesInfoCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+            )
+        } else {
+            val expandedNoteItem = remember { mutableStateOf<Note?>(null) }
+            LazyColumn(
+                contentPadding = PaddingValues(vertical = 24.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                items(items = uiState.notes, key = { note -> note.id }) { note ->
+                    val isExpanded = expandedNoteItem.value == note
+                    NoteListItem(
+                        note = note,
+                        isExpanded = isExpanded,
+                        onEditClick = onEditClick,
+                        onDeleteClick = onDeleteClick,
+                        onExpandClick = { expandedNoteItem.value = if (isExpanded) null else note },
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NoNotesInfoCard(
+    modifier: Modifier = Modifier,
+) {
+    ElevatedCard(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(top = 0.dp),
+                text = stringResource(R.string.notes_empty_info_card_title),
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                modifier = Modifier.padding(top = 0.dp),
+                text = stringResource(R.string.detail_notes_empty_info_card_content),
+                style = MaterialTheme.typography.bodyMedium,
             )
         }
     }
--- app/src/main/java/dev/zezula/books/ui/screen/detail/TabShelves.kt
+++ app/src/main/java/dev/zezula/books/ui/screen/detail/TabShelves.kt
@@ -1,19 +1,24 @@
 package dev.zezula.books.ui.screen.detail
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import dev.zezula.books.R
 import dev.zezula.books.data.model.shelf.ShelfForBook
 import dev.zezula.books.util.detailShelfCheckbox
 
@@ -22,6 +27,19 @@ fun TabShelves(
     uiState: BookDetailUiState,
     onShelfCheckedChange: (shelfForBook: ShelfForBook, isChecked: Boolean) -> Unit,
     modifier: Modifier = Modifier,
+) {
+    if (uiState.shelves.isEmpty()) {
+        NoShelvesInfoCard(modifier = modifier.padding(24.dp))
+    } else {
+        ShelvesList(modifier, uiState, onShelfCheckedChange)
+    }
+}
+
+@Composable
+private fun ShelvesList(
+    modifier: Modifier,
+    uiState: BookDetailUiState,
+    onShelfCheckedChange: (shelfForBook: ShelfForBook, isChecked: Boolean) -> Unit,
 ) {
     ElevatedCard(
         modifier = modifier
@@ -40,6 +58,31 @@ fun TabShelves(
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun NoShelvesInfoCard(
+    modifier: Modifier = Modifier,
+) {
+    ElevatedCard(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(top = 0.dp),
+                text = stringResource(R.string.detail_shelves_empty_info_card_title),
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                modifier = Modifier.padding(top = 0.dp),
+                text = stringResource(R.string.detail_shelves_empty_info_card_content),
+                style = MaterialTheme.typography.bodyMedium,
+            )
         }
     }
 }
